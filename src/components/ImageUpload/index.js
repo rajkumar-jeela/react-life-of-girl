42f689ae377e49ca1d299ecd3965788ec3018ec3
rajkumar-jeela <rajkumarjeela786@gmail.com>
2022-02-25 08:34:49 +0000

    sec commit

--- src/components/ImageUpload/index.js
+++ src/components/ImageUpload/index.js
@@ -1,22 +1,37 @@
 import './index.css'
 import {Component} from 'react'
+import axios from 'axios'
 
 class ImageUpload extends Component {
-  uploadImage = async event => {
-    event.preventDefault()
-    console.log(event.target)
-    const fr = new FileReader()
-    fr.onloadend = () => {
-      console.log(fr.result)
-    }
-    fr.readAsDataURL(event.target.files[0])
+  state = {
+    selectedFile: '',
   }
+
+  handleInputChange(event) {
+    const {files} = event.target
+    const reader = new FileReader()
+    reader.readAsDataURL(files[0])
+    reader.onload = e => {
+      this.setState({
+        selectedFile: e.target.result,
+      })
+      submit(event) {
+    event.preventDefault()
+
+    console.log(this.state)
+  }
+    }
+  }
+
+  handleInputChange = this.handleInputChange.bind(this)
+
+  
 
   render() {
     return (
       <div className="login-form-container">
-        <form className="form-container" onSubmit={this.uploadImage}>
-          <input type="file" />
+        <form className="form-container" onSubmit={this.submit}>
+          <input type="file" onChange={this.handleInputChange} />
           <button type="submit" className="login-button">
             Upload
           </button>
